2a08480318945bb762acdc85eed21ac1f73c2c13
CocoplamsDeveloper <it@cocopalms.io>
2023-10-08 17:08:45 +0300

    added decorator

--- facilitymanager/proplandtent/decorators.py
+++ facilitymanager/proplandtent/decorators.py
@@ -2,6 +2,26 @@ from functools import wraps
 import requests
 
 
-def is_authorized():
+def is_authorized(func):
 
-    def 
+    def wrapper_func(func):
+        @wraps(func)
+        def actual_func(*args, **kwargs):
+
+            try:
+                decoded_payload = jwt.decode(token, SECRET_KEY, audience=aud,algorithms=ALGORITHM)
+                return decoded_payload, 200
+                return func(*args, **kwargs)
+            except jwt.InvalidTokenError:
+                return {"message" :  "Invalid token"}, 403
+            except jwt.ExpiredSignatureError:
+                return {"message" : "token expired"}, 403
+            except jwt.InvalidAudienceError:
+                return {"message" : "Improper User"}, 403
+            except:
+                traceback.print_exc()
+                return {"message" : "server error"}, 500
+        
+        return actual_func
+
+    return wrapper_func
