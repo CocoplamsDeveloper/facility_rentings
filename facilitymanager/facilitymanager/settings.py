20add86fe962c58b8152a2a57c7f16474678dc79
CocoplamsDeveloper <it@cocopalms.io>
2023-10-31 17:37:33 +0300

    updated virtual env

--- facilitymanager/facilitymanager/settings.py
+++ facilitymanager/facilitymanager/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-'rentingsbackendapp-env.eba-3mz7jgda.me-south-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
